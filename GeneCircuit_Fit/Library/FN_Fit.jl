6729fe5ca7472c88bae17861e5142fbd0c79f559
Mariana Gómez-Schiavon <mgschiavon@gmail.com>
2021-08-03 14:46:31 -0500

    Adding basic library files

---
+++ GeneCircuit_Fit/Library/FN_Fit.jl
@@ -0,0 +1,75 @@
+## ODE FITTING PIPELINE - Gene Regulatory Circuit models
+#  ODE simulations & other functions
+#	Mariana Gómez-Schiavon
+#	August, 2021
+#		Julia v.1.5.3
+#		Local Environment: "./TOOLS/GeneCircuit_Fit/Project.toml"
+
+module fn
+	# Required libraries
+	using DifferentialEquations
+	using Distributions
+
+	# Steady state function for a given system
+	# INPUT: syst - Handle for the ODE system (@ode_def)
+	#        p    - Dictionary function with the ODE parameters & values
+	#        x0   - Vector of initial state of the ODE system
+	#        rtol - Tolerance value for ODE solver
+	# OUTPUT:     - Vector of steady state of the ODE system
+	function SS(syst, p, x0, rtol)
+		pV = [p[eval(Meta.parse(string(":",i)))] for i in syst.sys.ps];
+		tS = 0;
+		dXrm = 1;
+		while(dXrm > rtol)
+			ss = try
+				solve(ODEProblem(syst,x0,1e6,pV); reltol=rtol,save_everystep = false);
+			catch
+				try
+					solve(ODEProblem(syst,x0,1e6,pV),alg_hints=[:stiff]; reltol=rtol,save_everystep = false);
+				catch err
+					println("WARNING: Error in ODE simulation: <<",err,">>. ss --> NaN")
+					x0 = zeros(length(syst.syms)).+NaN;
+					break
+				end
+			end;
+			dXrm = maximum(abs.(big.(ss(1e6))-big.(ss(1e6-0.01)))./big.(ss(1e6)));
+			x0 = ss(1e6);
+			tS += 1e6;
+			if(tS>=1e12)
+				println("WARNING: Maximum iteration reached (simulated time 1e12). Max relative Delta: ",dXrm)
+				break
+			end
+		end
+		return x0
+	end;
+
+	# ODE dynamics for a given system
+	# INPUT: syst - Handle for the ODE system (@ode_def)
+	#        p    - Dictionary function with the ODE parameters & values
+	#        x0   - Vector of initial state of the ODE system
+	#        tspan- Time to simulate
+	#        rtol - Tolerance value for ODE solver
+	# OUPUT: xD   - ODE system solver
+	function Dyn(syst, p, x0, tspan, rtol)
+		pV = [p[eval(Meta.parse(string(":",i)))] for i in syst.sys.ps];
+		xD = try
+			solve(ODEProblem(syst,x0,tspan,pV); reltol=rtol);
+		catch
+			try
+				solve(ODEProblem(syst,x0,tspan,pV),alg_hints=[:stiff]; reltol=rtol);
+			catch err
+				println("WARNING: Error in ODE simulation: <<",err,">>. ss --> NaN")
+				zeros(length(syst.syms)).+NaN;
+			end
+		end;
+		return xD
+	end;
+
+	# Mean Squared Error (MSE) between model and data
+	# INPUT: Y - Vector of model values (i.e. steady state predictions)
+	#        D - Vector of data values (i.e. steady state measurements)
+	# OUPUT:   - Mean Squared Error
+	function MSE(Y,D)
+		return (sum((log10.(D) - log10.(Y)).^2)/std(log10.(D)))/length(Y);
+	end;
+end
---
+++ GeneCircuit_Fit/Library/Md_ToyEx1.jl
@@ -0,0 +1,19 @@
+## ODE FITTING PIPELINE - Gene Regulatory Circuit models
+# ODE model - Toy example #1
+#	Mariana Gómez-Schiavon
+#	August, 2021
+#		Julia v.1.5.3
+#		Local Environment: "./TOOLS/GeneCircuit_Fit/Project.toml"
+
+module mm
+	# Required libraries
+	using DifferentialEquations
+	using ParameterizedFunctions
+
+	# ODE system
+	myODE = @ode_def begin
+		dX  = (mX * Z) - (gX * X)
+		dY  = (mY * X) - (gY * Y)
+		dZ  = (mZ * Y) - (gZ * Z)
+	end mX gX mY gY mZ gZ;
+end
