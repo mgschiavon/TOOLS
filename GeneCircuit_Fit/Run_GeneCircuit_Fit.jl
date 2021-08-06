d7f87c0300e3e039919d01e6ca967121c620ea89
Mariana Gómez-Schiavon <mgschiavon@gmail.com>
2021-08-06 18:10:21 -0500

    First fitting test
    NOTE: It crashed on the third chain.
    'ERROR: LoadError: DomainError with -39520.87659441444:
    Exponentiation yielding a complex result requires a complex argument.
    Replace x^y with (x+0im)^y, Complex(x)^y, or similar.'

--- GeneCircuit_Fit/Run_GeneCircuit_Fit.jl
+++ GeneCircuit_Fit/Run_GeneCircuit_Fit.jl
@@ -1,6 +1,6 @@
 ## Running in julia terminal
 	cd("C:\\Users\\mgsch\\Dropbox (Personal)\\LIIGH\\TOOLS\\GeneCircuit_Fit\\")
 	using Pkg; Pkg.activate(".");
-	iARG = (mm = "ToyEx1",	# Label for model file
+	iARG = (mm = "UPR",	# Label for model file
 			ex = "Ex01");	# Label for parameters file
 	include("GeneCircuit_Fit.jl")
