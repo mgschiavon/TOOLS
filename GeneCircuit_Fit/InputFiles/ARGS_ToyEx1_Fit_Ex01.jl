34a2bfc23e2ebc12472f317e5741f366d16fa7b6
Mariana Gómez-Schiavon <mgschiavon@gmail.com>
2021-08-24 15:35:21 -0500

    Test - Fitting using DiffEqBayes.jl
    Toy Model, and not "clean" code yet.

--- GeneCircuit_Fit/InputFiles/ARGS_ToyEx1_Fit_Ex01.jl
+++ GeneCircuit_Fit/InputFiles/ARGS_ToyEx1_Fit_Ex01.jl
@@ -5,17 +5,23 @@
 #		Julia v.1.5.3
 #		Local Environment: "./TOOLS/GeneCircuit_Fit/Project.toml"
 
+using DiffEqBayes
+using ParameterizedFunctions, OrdinaryDiffEq, RecursiveArrayTools, Distributions
+
 # Fitting parameters
-mrw  = (pOp  = [:mX,:mY,:mZ],# Parameters to optimize
-		pMin = [-3,-3,-3],# Minimum parameter value to explore (log10)
-		pMax = [2,2,2],# Maximum parameter value to explore (log10)
-		runs = 4,# Number of optimization runs
-		iter = 100000,# Number of iterations per optimization run
-		cov  = [0.1,0.1,0.1],# Covariance to calculate parameter random walk
-		M    = 2,	# "Mutation step size" for multiplicative random walk
-		rnP0 = 0,	# Flag for random initial values of parameters to optimize
-		temp = 0,	# Flag for simulated annealing (if 0, MRW)
-		prtW = 1);	# Flag for printing each walk step
+pOp = Dict([
+	:mX => [5.8,5.8*0.2,0,100],       # X synthesis rate (1/min)
+	:mY => [2.1,2.1*0.2,0,100],       # Y synthesis rate (1/min)
+	:mZ => [0.05,0.05*0.2,0,100],     # Z synthesis rate (1/min)
+]);
+priors = [];
+for i in [eval(Meta.parse(string(":",i))) for i in mm.myODE.sys.ps]
+	if(in(i,keys(pOp)))
+		priors = vcat(priors,truncated(Normal(pOp[i][1], pOp[i][2]), pOp[i][3], pOp[i][4]));
+	else
+		priors = vcat(priors,truncated(Normal(p[i], p[i]*1e-4), 0, p[i]*100));
+	end
+end
 
 # Load data to compare:
 # NOTE: d.Xe is always the data matrix used to calculate the MSE.
@@ -25,13 +31,15 @@ x = CSV.File("DATA_ToyEx1.csv") |> Tables.matrix;
 d = (tD = x[1,2:end],		# Time points
 	 Xe = x[2:end,2:end]);	# Data points
 
-# RULES:
-function mySIM(fn,mm,p,d)
-	Y = zeros(size(d.Xe));
-	# Calculate dynamics:
-	xS = fn.Dyn(mm.myODE, p, x0, maximum(d.tD)*10.0, 1e-6);
-	for i in 1:length(d.tD)
-		Y[:,i] = xS(d.tD[i]);
-	end
-	return Y;
-end
+# Model:
+pV = [p[eval(Meta.parse(string(":",i)))] for i in mm.myODE.sys.ps];
+prob1 = ODEProblem(mm.myODE,x0,(0.0,maximum(d.tD)*10),pV);
+
+bayesian_result_hmc = dynamichmc_inference(prob1,Tsit5(),d.tD,d.Xe,priors)
+
+# Only Z:
+x = CSV.File("DATA_ToyEx1.csv") |> Tables.matrix;
+d = (tD = x[1,2:end],		# Time points
+	 Xe = x[end,2:end]);	# Data points
+
+bayesian_result_hmc2 = dynamichmc_inference(prob1,Tsit5(),d.tD,reshape(d.Xe,1,length(d.Xe)),priors,save_idxs=[3])
